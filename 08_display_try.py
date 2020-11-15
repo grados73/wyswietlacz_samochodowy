155f2f506fcd6188146e72e7c8901cf0cbed65f9
grados73 <71728314+grados73@users.noreply.github.com>
2020-11-15 19:20:07 +0100

    Dziala predkosc i tryby, ale w jednym okienku

---
+++ 08_display_try.py
@@ -0,0 +1,135 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+import time
+from kivy.uix.image import Image
+from kivy.clock import Clock
+import serial
+
+ser = serial.Serial('COM3', baudrate=9600, timeout=1)
+oldNum = 0
+
+
+class Background(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # create texture
+        self.tribe_texture = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\P_active.png").texture
+        print("pierwsze wyswietlenie")
+
+
+class Tribe(Widget):
+    tribe_texture = ObjectProperty(None)
+    speed_texture1 = ObjectProperty(None)
+    speed_texture10 = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # create texture INICJALIZACJA
+        self.tribe_texture = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\EN_active.png").texture #TRYBY JAZDY
+        self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture #PREDKOSC WARTOSC JEDNOSCI
+        self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture #PREDKOSC WARTOSC DZIESIĄTEK
+
+    def zmien_tryb(self, time_passed):
+        licznik = 1
+        oldNum = 0
+        num = 0
+## cześć dotycząca obrabiania danych z uart
+        if (ser.in_waiting > 0):
+            line = ser.readline()
+            a_string = line.decode("utf-8")  ##zmiana typu z bytes na string
+            for i in a_string:
+                if (i == '\r' or i == '\n' or i == '\r\n' or i == ' '):  ##omijam znaki nowej lini
+                    continue
+                num = int(a_string.rstrip())  ## ucinam spacje i zmieniam na int
+            if (oldNum != num):  ##jezli sie cos zmienilo
+                print(num)
+                oldNum = num ## num to jest końcowa wartosc otrzymana z uart
+## CZĘŚĆ DOTYCZĄCA RYSOWANIA NA WYSWIETLACZU - REAGOWANIE NA OTRZYMANE WIADOMOSCI
+    ## ZMIANA TRYBÓW JAZDY
+            if num == 401: #TRYB D
+                self.tribe_texture = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\D_active.png").texture
+            elif num == 411: #TRYB N
+                self.tribe_texture = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\EN_active.png").texture
+            elif num == 410: #TRYB R
+                self.tribe_texture = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\R_active.png").texture
+    ##ZMIANA PRĘDKOSCI
+        #WARTOSCI OD 0 DO 10 KM /H
+            if num >=200 and num <= 209:
+                self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture
+                if num == 200:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture
+                elif num == 201:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_1.png").texture
+                elif num == 202:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_2.png").texture
+                elif num == 203:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_3.png").texture
+                elif num == 204:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_4.png").texture
+                elif num == 205:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_5.png").texture
+                elif num == 206:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_6.png").texture
+                elif num == 207:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_7.png").texture
+                elif num == 208:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_8.png").texture
+                elif num == 209:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_9.png").texture
+        #WARTOSCI OD 10 DO 99 KM/H
+            elif num >= 210 and num <= 299:
+                predkosc = num - 200
+                if predkosc >= 90:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_9.png").texture
+                elif predkosc >= 80:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_8.png").texture
+                elif predkosc >= 70:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_7.png").texture
+                elif predkosc >= 60:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_6.png").texture
+                elif predkosc >= 50:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_5.png").texture
+                elif predkosc >= 40:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_4.png").texture
+                elif predkosc >= 30:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_3.png").texture
+                elif predkosc >= 20:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_2.png").texture
+                elif predkosc >= 10:
+                    self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_1.png").texture
+            #JEDNOSCI DLA WARTOSCI OD 10 DO 99 KM/H
+                jednosci = predkosc % 10
+                if jednosci == 0:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture
+                elif jednosci == 1:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_1.png").texture
+                elif jednosci == 2:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_2.png").texture
+                elif jednosci == 3:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_3.png").texture
+                elif jednosci == 4:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_4.png").texture
+                elif jednosci == 5:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_5.png").texture
+                elif jednosci == 6:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_6.png").texture
+                elif jednosci == 7:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_7.png").texture
+                elif jednosci == 8:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_8.png").texture
+                elif jednosci == 9:
+                    self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_9.png").texture
+
+class MainApp(App):
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.tribe.zmien_tryb, 1 / 10)
+#        Clock.schedule_interval(self.root.ids.speed.zmien_predkosc, 1 / 10)
+        pass
+
+    pass
+
+
+if __name__ == "__main__":
+    MainApp().run()
