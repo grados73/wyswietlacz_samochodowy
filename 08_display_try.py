c57183398a771e86869d98fdea86e3f881fefa76
grados73 <71728314+grados73@users.noreply.github.com>
2020-11-18 15:43:45 +0100

    a jednak się kręci
    na razie bez chaotycznie

--- 08_display_try.py
+++ 08_display_try.py
@@ -1,4 +1,4 @@
-import kivy
+#import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
@@ -6,9 +6,12 @@ import time
 from kivy.uix.image import Image
 from kivy.clock import Clock
 import serial
+from kivy.properties import NumericProperty
+from kivy.lang import Builder
 
 ser = serial.Serial('COM3', baudrate=9600, timeout=1)
 oldNum = 0
+cv = 0
 
 
 class Background(Widget):
@@ -19,7 +22,7 @@ class Background(Widget):
         print("pierwsze wyswietlenie")
 
 
-class Tribe(Widget):
+class Tribe(Widget, App):
     tribe_textureN = ObjectProperty(None)
     tribe_textureD = ObjectProperty(None)
     tribe_textureR = ObjectProperty(None)
@@ -29,6 +32,10 @@ class Tribe(Widget):
     turn_right = ObjectProperty(None)
     light_day = ObjectProperty(None)
     battery_lvl = ObjectProperty(None)
+    distance1 = ObjectProperty(None)
+    distance10 = ObjectProperty(None)
+    distance100 = ObjectProperty(None)
+    angle = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -37,6 +44,7 @@ class Tribe(Widget):
         self.tribe_textureD = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_tribe.jpg").texture #TRYBY JAZDY
         self.tribe_textureR = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_tribe.jpg").texture #TRYBY JAZDY
         self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture #PREDKOSC WARTOSC JEDNOSCI
+        self.angle = 0
         self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture #PREDKOSC WARTOSC DZIESIĄTEK
         self.turn_left = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_turn_left.jpg").texture #KIERUNKOWSKAZ LEWY
         self.turn_right = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_turn_right.jpg").texture #KIERUNKOWSKAZ PRAWY
@@ -62,6 +70,7 @@ class Tribe(Widget):
     ## ZMIANA TRYBÓW JAZDY
             if num == 401: #TRYB D
                 self.tribe_textureD = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\D_active.png").texture
+                self.tribe_textureD = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\D_active.png").texture
                 self.tribe_textureN = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_tribe.jpg").texture
                 self.tribe_textureR = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_tribe.jpg").texture
             elif num == 411: #TRYB N
@@ -78,24 +87,36 @@ class Tribe(Widget):
                 self.speed_texture10 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture
                 if num == 200:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_0.png").texture
+                    self.angle = 0
+                    global cv
+                    cv=1
                 elif num == 201:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_1.png").texture
+                    self.angle = 10
                 elif num == 202:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_2.png").texture
+                    self.angle = 20
                 elif num == 203:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_3.png").texture
+                    self.angle = 30
                 elif num == 204:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_4.png").texture
+                    self.angle = 40
                 elif num == 205:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_5.png").texture
+                    self.angle = 50
                 elif num == 206:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_6.png").texture
+                    self.angle = 60
                 elif num == 207:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_7.png").texture
+                    self.angle = 70
                 elif num == 208:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_8.png").texture
+                    self.angle = 0
                 elif num == 209:
                     self.speed_texture1 = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\speed_9.png").texture
+
         #WARTOSCI OD 10 DO 99 KM/H
             elif num >= 210 and num <= 299:
                 predkosc = num - 200
@@ -155,32 +176,12 @@ class Tribe(Widget):
                 self.light_day = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\empty_lights.jpg").texture
         #BATERIA
             elif num >= 300 and num <= 399:
-                if num >= 392:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_7.png").texture
-                elif num >= 385:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_6half.png").texture
-                elif num >= 378:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_6.png").texture
-                elif num >= 371:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_5half.png").texture
-                elif num >= 364:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_5.png").texture
-                elif num >= 357:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_4half.png").texture
-                elif num >= 350:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_4.png").texture
-                elif num >= 343:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_3half.png").texture
-                elif num >= 336:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_3.png").texture
-                elif num >= 329:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_2half.png").texture
-                elif num >= 322:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_2.png").texture
-                elif num >= 315:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_1half.png").texture
-                else:
-                    self.battery_lvl = Image(source="D:\Projekty\wyswietlacz_samochodowy\png\pbattery_1.png").texture
+                pass
+        #KĄT OBROTU
+
+        #NIEZNANA WIADOMOSC
+            else:
+                pass
 
 class MainApp(App):
     def on_start(self):
